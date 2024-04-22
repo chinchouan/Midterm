98cabe953299be69fe6f3b5a6e7353c983aeffaf
William <williamchen911031@gmail.com>
2024-04-22 18:20:17 +0800

    Change import describe

--- midterm.py
+++ midterm.py
@@ -1,17 +1,6 @@
 import os
 
-from pack.modu import (
-    add_books,
-    check_table,
-    create_db,
-    delete_books,
-    edit_books,
-    load_data,
-    login,
-    menu_builder,
-    search_books_data,
-    show_books,
-)
+import pack.modu as lib
 
 db_path = "./library.db"
 users_data_path = "./users.csv"
@@ -21,25 +10,25 @@ if __name__ == "__main__":
     db_exists = os.path.isfile(db_path)
     login_status = False
     if not db_exists:
-        create_db(db_path)
-        load_data(db_path, users_data_path, books_data_path)
+        lib.create_db(db_path)
+        lib.load_data(db_path, users_data_path, books_data_path)
     # Tables check
     # In this area try except include in the modu function
-    tables_exists = check_table(db_path)
+    tables_exists = lib.check_table(db_path)
     if not tables_exists:
-        create_db(db_path)
-        load_data(db_path, users_data_path, books_data_path)
+        lib.create_db(db_path)
+        lib.load_data(db_path, users_data_path, books_data_path)
     # login
     while not login_status:
         account = input("請輸入帳號：")
         password = input("請輸入密碼：")
-        login_status = login(db_path, account, password)
+        login_status = lib.login(db_path, account, password)
 
     # CURD
     # In this area try except include in choice
     active = True
     while active:
-        menu_builder()
+        lib.menu_builder()
         choice = input("選擇要執行的功能(Enter離開)：")
         if choice == "":
             active = False
@@ -59,7 +48,7 @@ if __name__ == "__main__":
                 if has_null_data:
                     print("=>給定的條件不足，無法進行新增作業")
                 else:
-                    add_books(db_path, datas)
+                    lib.add_books(db_path, datas)
             except ValueError:
                 print("年份請輸入整數")
             except Exception as e:
@@ -68,19 +57,19 @@ if __name__ == "__main__":
                 print(f'錯誤訊息為：{e.strerror}')
         elif choice == "2":
             try:
-                show_books(db_path)
+                lib.show_books(db_path)
                 delete_index = input("請問要刪除哪一本書？：")
                 if delete_index == "":
                     print("=>給定的條件不足，無法進行刪除作業")
                 else:
-                    delete_books(db_path, delete_index)
+                    lib.delete_books(db_path, delete_index)
             except Exception as e:
                 print('特定資料表顯示作業發生錯誤')
                 print(f'錯誤代碼為：{e.errno}')
                 print(f'錯誤訊息為：{e.strerror}')
         elif choice == "3":
             try:
-                show_books(db_path)
+                lib.show_books(db_path)
                 edit_index = input("請問要修改哪一本書的標題？：")
                 edit_title = input("請輸入要更改的標題：")
                 edit_author = input("請輸入要更改的作者：")
@@ -95,7 +84,7 @@ if __name__ == "__main__":
                 if has_null_data:
                     print("=>給定的條件不足，無法進行修改作業")
                 else:
-                    edit_books(db_path, edit_data)
+                    lib.edit_books(db_path, edit_data)
             except ValueError:
                 print("年份請輸入整數")
             except Exception as e:
@@ -105,14 +94,14 @@ if __name__ == "__main__":
         elif choice == "4":
             try:
                 select_index = input("請輸入想查詢的關鍵字：")
-                search_books_data(db_path, select_index)
+                lib.search_books_data(db_path, select_index)
             except Exception as e:
                 print('特定資料表顯示作業發生錯誤')
                 print(f'錯誤代碼為：{e.errno}')
                 print(f'錯誤訊息為：{e.strerror}')
         elif choice == "5":
             try:
-                show_books(db_path)
+                lib.show_books(db_path)
             except Exception as e:
                 print('資料表顯示作業發生錯誤')
                 print(f'錯誤代碼為：{e.errno}')
